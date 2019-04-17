f5e728b5aba0f1d384f1f64ebbf2e9849ca782ca
Can Berk Ocalir <cocalir@gmail.com>
2019-04-17 12:37:27 +0300

    committing files

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import './Sound/Sound';
+import './Timer/Timer';
+import './TimerControllers/TimerControllers';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Can's Pomodoro Timer</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TimerControllers/TimerControllers.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TimerControllers extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
