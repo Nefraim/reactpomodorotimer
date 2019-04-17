aeb62843e0e8551d75f14234285d8f2e933b3542
Can Berk Ocalir <cocalir@gmail.com>
2019-04-17 13:31:47 +0300

    Another commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,27 @@ import Sound from './Sound/Sound';
 import Timer from './Timer/Timer';
 import TimerControllers from './TimerControllers/TimerControllers';
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      cycle: "Session",
+      workTime: 25,
+      breakTime: 5,
+      sound: "on"
+    }
+  }
+    setSound = (sound) => {
+      this.setState({
+        sound: sound
+      })
+  }
   render() {
     return (
       <div className="App">
       <h1>Can's Pomodoro Timer</h1>
       <Timer/>
       <TimerControllers/>
-      <Sound/>
+      <Sound setSound={this.setSound} sound={this.state.sound}/>
       </div>
     );
   }
